2bce5d6f1088e8f0fe77484bc980c7885448078c
王庆 <1976331923@qq.com>
2021-07-28 15:37:52 +0800

    删除源文件

--- README.md
+++ README.md
@@ -1,30 +1,37 @@
-# vue3.0cli
+### vue3.0cli
 
-// axios ui库 echarts  
-## Project setup
+// axios ui 库 echarts
+
+### Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Run your unit tests
+
 ```
 npm run test:unit
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- vue.config.js
+++ vue.config.js
@@ -27,49 +27,44 @@ module.exports = {
           parallel: true
         }),
         new WebpackBundleAnalyzerPlugin(), // webpack 资源大小查看
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn$/),
         new CompressionPlugin({
           compressionOptions: { level: 9 },
           threshold: 10240, // 压缩超出10240字节大小
           algorithm: "gzip",
-          deleteOriginalAssets: false // 是否删除源文件
+          deleteOriginalAssets: true // 是否删除源文件
         })
       );
       config.optimization = {
         splitChunks: {
+          chunks: "all",
           cacheGroups: {
-            common: {
-              name: "chunk-common",
-              chunks: "initial",
-              minChunks: 1,
-              maxInitialRequests: 5,
-              minSize: 0,
-              priority: 1,
-              reuseExistingChunk: true,
-              enforce: true
+            vue: {
+              name: "vue",
+              test: /[\\/]node_modules[\\/]vue[\\/]/,
+              priority: -10
+            },
+            vuex: {
+              name: "vuex",
+              test: /[\\/]node_modules[\\/]vuex[\\/]/,
+              priority: -10
+            },
+            "vue-router": {
+              name: "vue-router",
+              test: /[\\/]node_modules[\\/]vue-router[\\/]/,
+              priority: -10
             },
             vendors: {
-              name: "chunk-vendors",
+              name: "vendors",
               test: /[\\/]node_modules[\\/]/,
-              chunks: "initial",
-              priority: 2,
-              reuseExistingChunk: true,
-              enforce: true
-            },
-            antd: {
-              name: "chunk-ant-design-vue",
-              test: /[\\/]node_modules[\\/]ant-design-vue[\\/]/,
-              chunks: "all",
-              priority: 3,
-              reuseExistingChunk: true,
-              enforce: true
-            },
-            elementUI: {
-              name: "chunk-elementUI-plus",
-              priority: 4,
-              chunks: "all",
-              test: /[\\/]node_modules[\\/]_?element-plus(.*)/,
-              enforce: true
+              priority: -20
             }
+            // "ant-design-vue": {
+            //   name: "ant-design-vue",
+            //   test: /[\\/]node_modules[\\/]ant-design-vue[\\/]/,
+            //   reuseExistingChunk: true,
+            //   enforce: true
+            // }
           }
         }
       };
@@ -81,8 +76,10 @@ module.exports = {
       .use(
         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn$/)
       );
-    // config.plugin("loadshReplace").use(new LodashModuleReplacementPlugin());
     config.resolve.alias.set("@", resolve("src"));
+    // 删除预加载
+    config.plugins.delete("preload");
+    config.plugins.delete("prefetch");
   },
   css: {
     // modules: false,
